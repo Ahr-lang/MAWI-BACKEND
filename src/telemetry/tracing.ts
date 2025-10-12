ee34d67257eaf73a0a3897f1b372785c1dab63ed
A01723229 <A01723229@tec.mx>
2025-10-12 14:58:16 -0600

    Actualizo backend con tracing y OpenTelemetry

--- src/telemetry/tracing.ts
+++ src/telemetry/tracing.ts
@@ -1,4 +1,3 @@
-// src/telemetry/tracing.ts
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
